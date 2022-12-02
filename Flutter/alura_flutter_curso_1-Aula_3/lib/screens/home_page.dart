f01626234764ee1e6101d54cbbdfcbac098ac35e
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-12-01 21:39:07 -0300

    TaskInherited

--- Flutter/alura_flutter_curso_1-Aula_3/lib/screens/home_page.dart
+++ Flutter/alura_flutter_curso_1-Aula_3/lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:alura_flutter_curso_1/components/task.dart';
+import 'package:alura_flutter_curso_1/screens/form_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,29 +19,24 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Tarefas'),
         leading: Container(),
       ),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: const Duration(milliseconds: 1000),
-        child: ListView(
-          children: const [
-            Tasks('Aprender Flutter', 'assets/dash.png', dificuldade: 3),
-            Tasks('Andar de Bike', 'assets/bike.webp', dificuldade: 2),
-            Tasks('Meditar', 'assets/meditar.jpeg', dificuldade: 5),
-            Tasks('Ler', 'assets/livro.jpg', dificuldade: 4),
-            Tasks('Jogar', 'assets/jogar.jpg', dificuldade: 1),
-            SizedBox(
-              height: 80,
-            )
-          ],
-        ),
+      body: ListView(
+        children: const [
+          Tasks('Aprender Flutter', 'assets/dash.png', dificuldade: 3),
+          Tasks('Andar de Bike', 'assets/bike.webp', dificuldade: 2),
+          Tasks('Meditar', 'assets/meditar.jpeg', dificuldade: 5),
+          Tasks('Ler', 'assets/livro.jpg', dificuldade: 4),
+          Tasks('Jogar', 'assets/jogar.jpg', dificuldade: 1),
+          SizedBox(
+            height: 80,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            opacidade = !opacidade;
-          });
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const FormScreen()));
         },
-        child: const Icon(Icons.remove_red_eye),
+        child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
