4efdb38e18dc315d5cfded8fd09acdac8426cd95
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-12-28 14:23:21 -0300

    parte 1 finalizado

--- Flutter/flutter_webapi_first_course-main/lib/main.dart
+++ Flutter/flutter_webapi_first_course-main/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/add_journal_screen/add_journal_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
@@ -6,7 +8,7 @@ import 'screens/home_screen/home_screen.dart';
 void main() {
   runApp(const MyApp());
   JournalService service = JournalService();
-  // service.register("Ola Mundo");
+  service.register(Journal.empty());
   // service.get();
 }
 
@@ -22,6 +24,8 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black,
           titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
           elevation: 0,
         ),
         textTheme: GoogleFonts.bitterTextTheme(),
@@ -31,6 +35,17 @@ class MyApp extends StatelessWidget {
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == 'add-journal') {
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(
+            builder: (context) {
+              return AddJournalScreen(journal: journal);
+            },
+          );
+        }
+        return null;
       },
     );
   }
